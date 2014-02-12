206910aa21d4f9ac1281d6012ec63d401c6fd960
kantale <alexandros.kanterakis@gmail.com>
2014-02-12 22:11:15 +0200

    removed Process Substitution from concat samples

--- compute5/Imputation_impute2/protocols/concatImpute2ChrResults_samples.sh
+++ compute5/Imputation_impute2/protocols/concatImpute2ChrResults_samples.sh
@@ -60,15 +60,29 @@ rm -f ${imputationIntermediatesFolder}/chr${chr}_${fromChrPos}-${toChrPos}_info
 
 #Merging chunks
 toExecute="paste <(cut -d ' ' -f 1-5 ${imputation__has__impute2ChunkOutput[0]})"
+concatCommandColumns="cut -d ' ' -f 1-5 ${imputation__has__impute2ChunkOutput[0]} > ${imputationIntermediatesFolder}/~chr${chr}_${fromChrPos}-${toChrPos}.columns"
+concatCommand="paste ${imputationIntermediatesFolder}/~chr${chr}_${fromChrPos}-${toChrPos}.columns "
+echo "Running: ${concatCommandColumns}
+eval ${concatCommandColumns}
+indexVar=0
 for element in ${imputation__has__impute2ChunkOutput[@]}
 do
+	indexVar=`expr $indexVar + 1`
 	toExecute="${toExecute} <( cut -d ' ' -f 6- ${element} )"
+	concatCommandColumns="cut -d ' ' -f 6- ${element} > ${imputationIntermediatesFolder}/~chr${chr}_${fromChrPos}-${toChrPos}.${indexVar}"
+	echo "Running: ${concatCommandColumns}"
+	eval ${concatCommandColumns}
+	concatCommand="${concatCommand} ${imputationIntermediatesFolder}/~chr${chr}_${fromChrPos}-${toChrPos}.${indexVar} "
 done
 toExecute="${toExecute} > ${imputationIntermediatesFolder}/~chr${chr}_${fromChrPos}-${toChrPos}"
+concatCommand="${concatCommand} > ${imputationIntermediatesFolder}/~chr${chr}_${fromChrPos}-${toChrPos}"
 
-echo "Executing: $toExecute"
-eval ${toExecute}
+# Process substitution is not available in all Unix shells
+#echo "Executing: $toExecute"
+#eval ${toExecute}
 
+echo "Executing: ${concatCommand}"
+eval ${concatCommand}
 
 returnCode=$?
 if [ $returnCode -eq 0 ]
