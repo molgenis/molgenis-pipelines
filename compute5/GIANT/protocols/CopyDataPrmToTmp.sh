e72b823da3108bc3924d2c7202695c093f5dc5eb
RoanKanninga <roankanninga@gmail.com>
2014-08-08 15:33:58 +0200

    split copytotmp and runcompute steps

--- compute5/GIANT/protocols/CopyDataPrmToTmp.sh
+++ compute5/GIANT/protocols/CopyDataPrmToTmp.sh
@@ -32,25 +32,3 @@ echo 'copied: ${chrX_1000G_auto_phase1_refpanel}* ${GIANT_workDir_originalFiles}
 
 cp ${chrX_1000G_sampleIDs_female_1000G} ${GIANT_workDir_originalFiles}/${chrX_1000G_sampleIDs_female_1000G_Name}
 echo 'cp ${chrX_1000G_sampleIDs_female_1000G} ${GIANT_workDir_originalFiles}/${chrX_1000G_sampleIDs_female_1000G_Name}'
-
-module load molgenis_compute/v5_20140522
-module list
-
-if [ -f .compute.properties ];
-then
-     rm .compute.properties
-fi
-
-sh convert.sh ${MC_HOME}/GIANT_pipeline/parameters_not_converted.csv ${MC_HOME}/GIANT_pipeline/parameters.csv
-
-GCC_HOME=/gcc
-sh ${MC_HOME}/molgenis_compute.sh \
--p ${MC_HOME}/GIANT_pipeline/parameters.csv \
--w ${MC_HOME}/GIANT_pipeline/GIANT_workflow.csv \
--b pbs \
--rundir ${GIANT_workDir}/jobs \
---runid test02 \
---weave \
---generate
-
-echo "creating jobs finished succesfully. You can now run ${GIANT_workDir}/jobs/submit.sh"
