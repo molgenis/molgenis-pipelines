3068768d2ea94b28c415af554611f812a0f4ac58
mdijkstra <mdijkstrasEmailAddressIsHidden>
2014-10-28 15:25:08 +0100

    scrit that knits the example into out/

--- compute5/NGS_alignment_SNP_calling/report/minimal/doit.R
+++ compute5/NGS_alignment_SNP_calling/report/minimal/doit.R
@@ -1,3 +1,8 @@
 library(knitr)
-knit('001-minimal.Rmd',output='out/001-minimal.md')
+
+path.output	= '~/Documents/git/molgenis-pipelines/compute5/NGS_alignment_SNP_calling/report/minimal/out/' # update for your case
+file.input	= '../001-minimal.Rmd'
+
+setwd(path.output) # because figs need to be next to output
+knit(file.input)
 
