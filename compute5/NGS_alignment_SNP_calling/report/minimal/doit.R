1e085b8491da902891b40e8a1fea8d7cd9db245b
mdijkstra <mdijkstrasEmailAddressIsHidden>
2014-10-28 16:25:16 +0100

    Updated minimal example

--- compute5/NGS_alignment_SNP_calling/report/minimal/doit.R
+++ compute5/NGS_alignment_SNP_calling/report/minimal/doit.R
@@ -4,5 +4,11 @@ path.output	= '~/Documents/git/molgenis-pipelines/compute5/NGS_alignment_SNP_cal
 file.input	= '../001-minimal.Rmd'
 
 setwd(path.output) # because figs need to be next to output
+
+# Get out/001-minimal.md file
 knit(file.input)
 
+# Get out/001-minimal.R file with R commands, including different levels of doc
+#purl(file.input, documentation = 0L)
+#purl(file.input, documentation = 1L)
+#purl(file.input, documentation = 2L)
