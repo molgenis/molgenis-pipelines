f729c2146f8cf31f289f9134af388d2d5e0a1968
Gerben van der Vries <Gerbenvandervries@gmail.com>
2014-10-30 17:29:14 +0100

    splitted indels per sample, and merged pindel and gatk.

--- compute5/NGS_alignment_SNP_calling/protocols/IndelMergeVcf.sh
+++ compute5/NGS_alignment_SNP_calling/protocols/IndelMergeVcf.sh
@@ -6,30 +6,23 @@
 #string intermediateDir
 #string project
 #string projectIndelsMerged
-#list externalSampleID
-
-#Load Bcftools module
-${stage} bcftools/0.2.0
-
-#Load Tabix module
-${stage} tabix/0.2.6
+#string externalSampleID
+#string indexFile
+#string sampleIndelsPindelGATKMerged
+#string targetIntervals
+ 
+#Load GATK,bcftools,tabix module
+${stage} GATK/3.2-2-gec30cee
+module load bcftools/0.2.0
+module load tabix/0.2.6
 ${checkStage}
 
 #Echo parameter values
 echo "stage: ${stage}"
 echo "checkStage: ${checkStage}"
 
-makeTmpDir ${intermediateDir}
-tmpIntermediateDir=${MC_tmpFile}
-
-#prepare the created vcf's for bcftools: bgzip + tabix to set the correct indexes and make correct format
-
-for externalSample in "${externalSampleID[@]}"
-do
-  echo "externalSampleID: ${externalSample}"
-done
-
-sleep 10
+makeTmpDir ${sampleIndelsPindelGATKMerged}
+tmp_sampleIndelsPindelGATKMerged=${MC_tmpFile}
 
 #Function to check if array contains value
 array_contains () {
@@ -44,40 +37,42 @@ array_contains () {
     done
     return $in
 }
-vcfTmpDir="${tmpIntermediateDir}/vcfTmpDir"
 
-if [ ! -d ${vcfTmpDir ]
-then 
-	mkdir ${vcfTmpDir}
-fi
+echo "running GATK : SelectVariants"
 
-INPUTS=()
-for sample in "${externalSampleID[@]}"
-do
-        array_contains INPUTS "INPUT=$sample" || INPUTS+=("INPUT=$sample")    # If sample does not exist in array add it
-done
+#Select a sample and restrict the output vcf to a set of intervals:
+ java -Xmx2g -jar $GATK_HOME/GenomeAnalysisTK.jar \
+   -R ${indexFile} \
+   -T SelectVariants \
+   --variant ${projectIndelsMerged} \
+   -o ${intermediateDir}/${externalSampleID}.indels.GATK.vcf \
+   -L ${targetIntervals} \
+   -sn ${externalSampleID}
 
-for s in "${INPUTS[@]}"
-do
-	bgzip -c ${intermediateDir}/${s}.output.pindel.merged.vcf > ${tmpIntermediateDir}/${s}.output.pindel.merged.vcf.gz
-        tabix -p vcf ${tmpIntermediateDir}/${s}.output.pindel.merged.vcf.gz
-        echo ${tmpIntermediateDir}/${s}; 
-	cp ${tmpIntermediateDir}/${s}.output.pindel.merged.vcf.gz ${vcfTmpDir}
-done
+#gzip and make indexfiles for bcftools
+bgzip -c ${intermediateDir}/${externalSampleID}.indels.GATK.vcf > ${intermediateDir}/${externalSampleID}.indels.GATK.vcf.gz
+bgzip -c ${intermediateDir}/${externalSampleID}.output.pindel.merged.vcf > ${intermediateDir}/${externalSampleID}.output.pindel.merged.vcf.gz
+tabix -p vcf ${intermediateDir}/${externalSampleID}.indels.GATK.vcf.gz
+tabix -p vcf ${intermediateDir}/${externalSampleID}.output.pindel.merged.vcf.gz
+
+echo "running BCFTools : merge"
+bcftools merge \
+${intermediateDir}/${externalSampleID}.indels.GATK.vcf.gz \
+${intermediateDir}/${externalSampleID}.output.pindel.merged.vcf.gz \
+--output-type v > ${tmp_sampleIndelsPindelGATKMerged}
+
+#echo header into outputfile, and remove collumn headers
+bcftools view -h ${tmp_sampleIndelsPindelGATKMerged} > ${tmp_sampleIndelsPindelGATKMerged}.tmp
+sed -i '$ d' ${tmp_sampleIndelsPindelGATKMerged}.tmp
+
+#create new header and replace header of output vcf using tabix
+echo "#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	${externalSampleID}.GATK	${externalSampleID}.PINDEL" > ${intermediateDir}/${externalSampleID}.header.txt
+bgzip -c ${tmp_sampleIndelsPindelGATKMerged} > ${tmp_sampleIndelsPindelGATKMerged}.gz
+tabix -p vcf ${tmp_sampleIndelsPindelGATKMerged}.gz
+tabix -r ${intermediateDir}/${externalSampleID}.header.txt ${tmp_sampleIndelsPindelGATKMerged}.gz >> ${tmp_sampleIndelsPindelGATKMerged}.reheadered.tmp.gz
+gunzip ${tmp_sampleIndelsPindelGATKMerged}.reheadered.tmp.gz
+cat ${tmp_sampleIndelsPindelGATKMerged}.reheadered.tmp >> ${tmp_sampleIndelsPindelGATKMerged}.tmp
 
 
-cp ${projectIndelsMerged}.gz ${vcfTmpDir}
-
-cd ${vcfTmpDir}
-#merging all the vcf.gz that were created per sample into one big vcf
-echo "running bcftools:"
-bcftools merge *.vcf.gz --output-type v > ${tmpIntermediateDir}${project}.indels.calls.mergedAllVcf.vcf
-
-echo "written ${project}.indels.calls.mergedAllVcf.vcf TO ${tmpIntermediateDir}"
-
-mv ${tmpIntermediateDir}${project}.indels.calls.mergedAllVcf.vcf ${intermediateDir}${project}.indels.calls.mergedAllVcf.vcf
-
-if [ -d ${vcfTmpDir} ]
-then
-	rm -rf ${vcfTmpDir}
-fi
+mv ${tmp_sampleIndelsPindelGATKMerged}.tmp ${tmp_sampleIndelsPindelGATKMerged}
+mv ${tmp_sampleIndelsPindelGATKMerged} ${sampleIndelsPindelGATKMerged}
