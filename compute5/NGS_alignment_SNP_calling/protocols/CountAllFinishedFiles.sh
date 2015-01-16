a89a0fca374d1cf79eeebd1e0bca9194314f6b27
RoanKanninga <roankanninga@gmail.com>
2015-01-16 08:42:01 +0100

    fixed the countallfinishedfiles protocol + added JavaVersion variable to CreateInhouseProjects

--- compute5/NGS_alignment_SNP_calling/protocols/CountAllFinishedFiles.sh
+++ compute5/NGS_alignment_SNP_calling/protocols/CountAllFinishedFiles.sh
@@ -4,22 +4,24 @@
 cd $projectJobsDir
 
 countShScripts=`ls *.sh | wc -l`
-countFinishedFiles=`ls *.finished | wc -l`
+countFinishedFiles=`ls *.sh.finished | wc -l`
 
-#remove 1, because this step is not finished yet
-countShScripts=$(($countShScripts-1))
+#remove 2, because this step (CountAllFinishedFiles) is not finished yet and there is submit.sh
+countShScripts=$(($countShScripts-2))
 
-rm ${projectJobsDir}/${taskId}_INCORRECT
+rm -f ${projectJobsDir}/${taskId}_INCORRECT
 
-if (( $countShScripts == $finishedFiles ))
+if (( $countShScripts == $countFinishedFiles ))
 then	
 	echo "all files are finished" > ${projectJobsDir}/${taskId}_CORRECT
 else
 	
 	for getSh in $(ls *.sh)
 	do
-		ls ${getSh}.finished >> ${projectJobsDir}/${taskId}_INCORRECT
-
+		if [ ! -f ${getSh}.finished ]
+		then
+			${getSh}.finished >> ${projectJobsDir}/${taskId}_INCORRECT
+		fi
 	done
 fi
 	
