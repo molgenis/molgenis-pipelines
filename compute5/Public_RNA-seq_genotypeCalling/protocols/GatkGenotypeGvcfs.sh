18b734ac3cac3973fa702d50aa02ae31a8766d54
freerkvandijk <freerk.van.dijk@gmail.com>
2015-05-15 14:35:43 +0200

    RNA-seq protocols

---
+++ compute5/Public_RNA-seq_genotypeCalling/protocols/GatkGenotypeGvcfs.sh
@@ -0,0 +1,62 @@
+#MOLGENIS walltime=23:59:00 mem=8gb ppn=4
+
+#Parameter mapping  #why not string foo,bar? instead of string foo\nstring bar
+#string stage
+#string checkStage
+#string starVersion
+#string WORKDIR
+#string projectDir
+#string dbsnpVcf
+#string dbsnpVcfIdx
+
+#string gatkVersion
+#string haplotyperDir
+#string onekgGenomeFasta
+
+#Array reference because it's possible
+#list mergeGvcf
+#list mergeGvcfIdx
+
+#string genotypedVcf
+#string genotypedVcfIdx
+
+echo "## "$(date)" ##  $0 Started "
+
+
+for file in "${mergeGvcf[@]}" "${mergeGvcfIdx[@]}" "${onekgGenomeFasta}"; do
+	echo "getFile file='$file'"
+	getFile $file
+done
+
+#Load gatk module
+${stage} GATK/${gatkVersion}
+${checkStage}
+
+set -x
+set -e
+
+
+# sort unique and print like ' --variant file1.vcf --variant file2.vcf '
+gvcfs=($(printf '%s\n' "${mergeGvcf[@]}" | sort -u ))
+
+inputs=$(printf ' --variant %s ' $(printf '%s\n' ${gvcfs[@]}))
+
+mkdir -p ${haplotyperDir}
+
+#pseudo: java -jar GenomeAnalysisTK.jar -T HaplotypeCaller -R ref.fasta -I input.bam -recoverDanglingHeads -dontUseSoftClippedBases -stand_call_conf 20.0 -stand_emit_conf 20.0 -o output.vcf from http://gatkforums.broadinstitute.org/discussion/3891/calling-variants-in-rnaseq
+
+java -Xmx4g -Djava.io.tmpdir=${haplotyperDir} -jar $GATK_HOME/GenomeAnalysisTK.jar \
+ -T GenotypeGVCFs \
+ -R ${onekgGenomeFasta} \
+ --dbsnp ${dbsnpVcf}\
+ -o ${genotypedVcf} \
+ $inputs \
+ -stand_call_conf 10.0 \
+ -stand_emit_conf 20.0 \
+ -nt 4
+
+
+putFile ${genotypedVcf}
+putFile ${genotypedVcfIdx}
+
+echo "## "$(date)" ##  $0 Done "I
