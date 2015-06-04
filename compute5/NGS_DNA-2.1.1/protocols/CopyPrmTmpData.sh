1079f2c8f2c9a1ebba412d85d54cd0c5d663a46a
RoanKanninga <roankanninga@gmail.com>
2015-06-04 13:25:57 +0200

    minor bugfix

--- compute5/NGS_DNA-2.1.1/protocols/CopyPrmTmpData.sh
+++ compute5/NGS_DNA-2.1.1/protocols/CopyPrmTmpData.sh
@@ -34,17 +34,17 @@ do
   		mkdir -p ${TMPDATADIR}
 		if [[ ${barcode[samplenumber]} == "None" ]]
 		then
-			rsync -a -r \
+			rsync -a -r --no-perms \
 			${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}.fq.gz \
 			${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}.fq.gz
-			rsync -a -r \
+			rsync -a -r --no-perms \
 			${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}.fq.gz.md5 \
 			${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}.fq.gz.md5
 		else
-			rsync -a -r \
+			rsync -a -r --no-perms \
 			${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}.fq.gz \
 			${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}.fq.gz
-			rsync -a -r \
+			rsync -a -r --no-perms \
 			${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}.fq.gz.md5 \
 			${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}.fq.gz.md5	
 		fi
@@ -53,29 +53,29 @@ do
 		mkdir -p ${TMPDATADIR}
 		if [[ ${barcode[samplenumber]} == "None" ]]
     		then
-    		rsync -a -r \
+    		rsync -a -r --no-perms \
     			${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_1.fq.gz \
     			${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_1.fq.gz
-		rsync -a -r \
+		rsync -a -r --no-perms \
 			${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_2.fq.gz \
 			${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_2.fq.gz
-		rsync -a -r \
+		rsync -a -r --no-perms \
 			${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_1.fq.gz.md5 \
 			${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_1.fq.gz.md5
-        	rsync -a -r \
+        	rsync -a -r --no-perms \
         		${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_2.fq.gz.md5 \
         		${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_2.fq.gz.md5
 		else          
-        	rsync -a -r \
+        	rsync -a -r --no-perms \
         		${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}_1.fq.gz \
         		${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}_1.fq.gz
-        	rsync -a -r \
+        	rsync -a -r --no-perms \
         		${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}_2.fq.gz \
         		${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}_2.fq.gz
-        	rsync -a -r \
+        	rsync -a -r --no-perms \
         		${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}_1.fq.gz.md5 \
         		${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}_1.fq.gz.md5
-        	rsync -a -r \
+        	rsync -a -r --no-perms \
         		${PRMDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}_2.fq.gz.md5 \
         		${TMPDATADIR}/${RUNNAME}_L${lane[samplenumber]}_${barcode[samplenumber]}_2.fq.gz.md5
     		fi
