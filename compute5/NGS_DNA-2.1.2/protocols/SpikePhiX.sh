1843122f32eed1e9d1e9ff7c392597b3e49ebaa2
RoanKanninga <roankanninga@gmail.com>
2015-06-29 15:00:12 +0200

    included gVCF variant calling in the pipeline

---
+++ compute5/NGS_DNA-2.1.2/protocols/SpikePhiX.sh
@@ -0,0 +1,40 @@
+#MOLGENIS nodes=1 ppn=1 mem=1gb walltime=12:30:00
+
+#Parameter mapping
+#string seqType
+#string phiXEnd1Gz
+#string phiXEnd2Gz
+#string srBarcodeFqGz
+#string peEnd1BarcodeFqGz
+#string peEnd2BarcodeFqGz
+#string project
+
+#Echo parameter values
+echo "seqType: ${seqType}"
+
+sleep 10 
+
+module load pigz/2.3.3
+
+# Spike phiX only once
+samp=`tail -10 ${peEnd1BarcodeFqGz}`
+phiX=`tail -10 ${phiXEnd1Gz}`
+
+if [ "$samp" = "$phiX" ]; 
+then
+	echo "Skip this step! PhiX was already spiked in!"
+	exit 0
+else
+	if [ ${seqType} == "SR" ]
+	then
+		echo "Spike phiX not implemented yet for Single Read"
+		exit 1
+	elif [ $seqType == "PE" ]
+	then
+		echo "Append phiX reads"
+		cat ${peEnd1BarcodeFqGz} ${phiXEnd1Gz} > ${peEnd1BarcodeFqGz}.tmp
+		cat ${peEnd2BarcodeFqGz} ${phiXEnd2Gz} > ${peEnd2BarcodeFqGz}.tmp
+		mv ${peEnd1BarcodeFqGz}.tmp ${peEnd1BarcodeFqGz}
+		mv ${peEnd2BarcodeFqGz}.tmp ${peEnd2BarcodeFqGz}
+	fi
+fi
