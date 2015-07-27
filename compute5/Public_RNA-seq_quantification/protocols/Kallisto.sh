ec149bb3cb7cf22d3913816041ecaade1e3dbe2c
npklein <niekdeklein@gmail.com>
2015-07-27 22:02:20 +0200

    quantification fixed

--- compute5/Public_RNA-seq_quantification/protocols/Kallisto.sh
+++ compute5/Public_RNA-seq_quantification/protocols/Kallisto.sh
@@ -18,41 +18,45 @@
 getFile ${reads1FqGz}
 
 #Load modules
-${stage} kallisto/${kallistoVersion}
+${stage} Kallisto/${kallistoVersion}
 
 #check modules
 ${checkStage}
-
+internalIdArray=(${internalId//_/ })
 
 echo "## "$(date)" Start $0"
 echo "ID (internalId-project-sampleName): ${internalId}-${project}-${sampleName}"
-
+echo ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}
 if [ ${#reads2FqGz} -eq 0 ]; then
-  mkdir -p ${kallistoDir}_200
+  mkdir -p ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}
   echo "Single end kallisto of ${reads1FqGz}"
   if kallisto quant \
     -i ${kallistoIndex} \
-    -o ${kallistoDir}/SRP034691/SRS518677/SRR1057370_${fragmentLength} \
+    -o ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength} \
     --single \
     -l 200 \
     ${reads1FqGz}
   then
-    echo "returncode: $?"; putFile ${hisatAlignmentDir}${uniqueID}.sam
+    echo "returncode: $?"; putFile ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}/abundance.txt
+    putFile ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}/abundance.h5
+    putFile ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}/run_info.json
     echo "succes moving files";
   else
     echo "returncode: $?";
     echo "fail";
   fi
 else
-  mkdir -p ${kallistoDir}
+  mkdir -p ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}
   getFile ${reads2FqGz}
   echo "Paired end kallisto of ${reads1FqGz} and ${reads2FqGz}"
   if kallisto quant \
     -i ${kallistoIndex} \
-    -o ${kallistoDir}/ERP006215/ERS529346/ERR737789 \
+    -o ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength} \
     ${reads1FqGz} ${reads2FqGz}
   then
-    echo "returncode: $?"; putFile ${hisatAlignmentDir}${uniqueID}.sam
+    echo "returncode: $?"; putFile ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}/abundance.txt
+    putFile ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}/abundance.h5
+    putFile ${kallistoDir}/${internalIdArray[1]}/${internalIdArray[2]}/${internalIdArray[3]}_${fragmentLength}/run_info.json
     echo "succes moving files";
   else
     echo "returncode: $?";
