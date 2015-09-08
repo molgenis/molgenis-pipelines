9079250b9aa47b5b2c9940aaf91ff325e4f9bf0d
NPK <niekdeklein@gmail.com>
2015-09-08 17:27:09 +0200

    Homer visualization and peakfinding refinement PROseq

---
+++ compute5/PROseq/protocols/HomerFindPeaks.sh
@@ -0,0 +1,37 @@
+#MOLGENIS walltime=23:59:00 mem=12gb ppn=8
+
+### variables to help adding to database (have to use weave)
+#string sampleName
+#string project
+###
+#string stage
+#string checkStage
+#string homerTagDir
+#string toolDir
+#string homerLocation
+#string weblogoVersion
+#string samtoolsVersion
+
+echo "## "$(date)" Start $0"
+echo "ID (project-sampleName): ${project}-${sampleName}"
+
+# bad! But Homer is near impossible to install with easybuild so can't use module load
+PATH=$PATH:${homerLocation}
+PATH=$PATH:${bedgraphBigwigLocation}
+
+
+#Load gatk module
+${stage} SAMtools/${samtoolsVersion}
+${stage} Weblogo/${weblogoVersion}
+${checkStage}
+
+if findPeaks ${homerTagDir} -style groseq -o auto
+then
+ echo "returncode: $?"; 
+ echo "succes moving files";
+else
+ echo "returncode: $?";
+ echo "fail";
+fi
+
+echo "## "$(date)" ##  $0 Done "
