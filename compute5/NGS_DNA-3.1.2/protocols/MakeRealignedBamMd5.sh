e88dccf0d060abe613244c6b349abe1ec3f417d8
gerbenvandervries <gerbenvandervries@gmail.com>
2015-09-14 14:00:35 +0200

    bugfix memorie, and snpeff version

---
+++ compute5/NGS_DNA-3.1.2/protocols/MakeRealignedBamMd5.sh
@@ -0,0 +1,7 @@
+#MOLGENIS walltime=23:59:00 mem=4gb
+
+#string realignedBam
+
+md5sum ${realignedBam} > ${realignedBam}.md5
+
+
---
+++ compute5/NGS_DNA-3.1.2/protocols/MergeBatches.sh
@@ -0,0 +1,70 @@
+#MOLGENIS walltime=23:59:00 mem=4gb ppn=2
+
+#Parameter mapping
+#string stage
+#string checkStage
+#string gatkVersion
+#string gatkJar
+#string tabixVersion
+#string tempDir
+#string intermediateDir
+#string projectVariantsMerged
+#string projectVariantsMergedSorted
+#list batchID
+#string projectPrefix
+#string tmpDataDir
+#string project
+#string sortVCFpl
+#string indexFile
+#string indexFileFastaIndex
+
+#Load module GATK,tabix
+${stage} ${gatkVersion}
+${stage} ${tabixVersion}
+${stage} ngs-utils
+${checkStage}
+
+makeTmpDir ${projectVariantsMerged}
+tmpProjectVariantsMerged=${MC_tmpFile}
+
+makeTmpDir ${projectVariantsMergedSorted}
+tmpProjectVariantsMergedSorted=${MC_tmpFile}
+
+#Function to check if array contains value
+array_contains () {
+    local array="$1[@]"
+    local seeking=$2
+    local in=1
+    for element in "${!array-}"; do
+        if [[ "$element" == "$seeking" ]]; then
+            in=0
+            break
+        fi
+    done
+    return $in
+}
+
+INPUTS=()
+
+for b in "${batchID[@]}"
+do
+	if [ -f ${projectPrefix}.batch-${b}.variant.calls.combined.genotyped.vcf ]
+	then
+		array_contains INPUTS "--variant ${projectPrefix}.batch-${b}.variant.calls.combined.genotyped.vcf" || INPUTS+=("--variant ${projectPrefix}.batch-${b}.variant.calls.combined.genotyped.vcf")
+	fi
+done
+
+java -cp ${EBROOTGATK}/${gatkJar} org.broadinstitute.gatk.tools.CatVariants \
+-R ${indexFile} \
+${INPUTS[@]} \
+-out ${tmpProjectVariantsMerged} \
+-assumeSorted
+
+#sort and rename VCF file 
+${sortVCFpl} \
+-fastaIndexFile ${indexFileFastaIndex} \
+-inputVCF ${tmpProjectVariantsMerged} \
+-outputVCF ${tmpProjectVariantsMergedSorted}
+
+mv ${tmpProjectVariantsMergedSorted} ${projectVariantsMergedSorted}
+echo "mv ${tmpProjectVariantsMergedSorted} ${projectVariantsMergedSorted}"
