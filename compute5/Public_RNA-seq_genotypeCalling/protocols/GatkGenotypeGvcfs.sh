d8dfd70baa051a997f0310264cd04f2385b30ae3
freerkvandijk <freerk.van.dijk@gmail.com>
2015-10-14 17:05:22 +0200

    Updated genotypeCalling pipeline for haplotypecaller per chromosome

--- compute5/Public_RNA-seq_genotypeCalling/protocols/GatkGenotypeGvcfs.sh
+++ compute5/Public_RNA-seq_genotypeCalling/protocols/GatkGenotypeGvcfs.sh
@@ -14,16 +14,16 @@
 #string gatkVersion
 #string haplotyperDir
 #string onekgGenomeFasta
-#list mergeGvcf
+#list mergeChrGvcf
 
-#string genotypedVcf
-#string genotypedVcfIdx
+#string genotypedChrVcf
+#string genotypedChrVcfIdx
 #string toolDir
 
 echo "## "$(date)" Start $0"
 
-#for file in "${mergeGvcf[@]}" "${mergeGvcfIdx[@]}" "${onekgGenomeFasta}"; do
-for file in "${mergeGvcf[@]}" "${onekgGenomeFasta}"; do
+#for file in "${mergeChrGvcf[@]}" "${mergeChrGvcfIdx[@]}" "${onekgGenomeFasta}"; do
+for file in "${mergeChrGvcf[@]}" "${onekgGenomeFasta}"; do
     echo "getFile file='$file'"
 	getFile $file
 done
@@ -34,7 +34,7 @@ ${checkStage}
 
 
 # sort unique and print like ' --variant file1.vcf --variant file2.vcf '
-gvcfs=($(printf '%s\n' "${mergeGvcf[@]}" | sort -u ))
+gvcfs=($(printf '%s\n' "${mergeChrGvcf[@]}" | sort -u ))
 
 inputs=$(printf ' --variant %s ' $(printf '%s\n' ${gvcfs[@]}))
 
@@ -44,7 +44,7 @@ if java -Xmx8g -XX:ParallelGCThreads=8 -Djava.io.tmpdir=${haplotyperDir} -jar ${
  -T GenotypeGVCFs \
  -R ${onekgGenomeFasta} \
  --dbsnp ${dbsnpVcf} \
- -o ${genotypedVcf} \
+ -o ${genotypedChrVcf} \
  $inputs \
  -stand_call_conf 10.0 \
  -stand_emit_conf 20.0 \
@@ -53,8 +53,8 @@ if java -Xmx8g -XX:ParallelGCThreads=8 -Djava.io.tmpdir=${haplotyperDir} -jar ${
 then
  echo "returncode: $?"; 
  
- putFile ${genotypedVcf}
- putFile ${genotypedVcfIdx}
+ putFile ${genotypedChrVcf}
+ putFile ${genotypedChrVcfIdx}
  echo "succes moving files";
 else
  echo "returncode: $?";
