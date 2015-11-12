9be10fefa55663f2f323d0703259a9622af7954b
RoanKanninga <roankanninga@gmail.com>
2015-11-12 15:08:06 +0100

    fixed some Metrics protocols + samToBam bugfix

--- compute5/NGS_DNA/protocols/CollectBamIndexMetrics.sh
+++ compute5/NGS_DNA/protocols/CollectBamIndexMetrics.sh
@@ -8,16 +8,16 @@
 #string bamIndexStatsJar
 #string dedupBam
 #string dedupBamIdx
-#string collectBamMetricsPrefix
 #string tempDir
 #string capturingKit
 #string picardJar
+#string bamIndexStats
 
 #Load Picard module
 ${stage} ${picardVersion}
 
-makeTmpDir ${collectBamMetricsPrefix}
-tmpCollectBamMetricsPrefix=${MC_tmpFile}
+makeTmpDir ${bamIndexStats}
+tmpBamIndexStats=${MC_tmpFile}
 
 
 #Run Picard BamIndexStats
@@ -25,8 +25,8 @@ java -jar -Xmx4g ${EBROOTPICARD}/${picardJar} ${bamIndexStatsJar} \
 INPUT=${dedupBam} \
 VALIDATION_STRINGENCY=LENIENT \
 TMP_DIR=${tempDir} \
-> ${tmpCollectBamMetricsPrefix}.bam_index_stats
+> ${tmpBamIndexStats}
 
-echo -e "\nBamIndexStats finished succesfull. Moving temp files to final.\n\n"
-mv ${tmpCollectBamMetricsPrefix}.bam_index_stats ${dedupBam}.bam_index_stats
+mv ${tmpBamIndexStats} ${bamIndexStats}
+echo "moved ${tmpBamIndexStats} to ${bamIndexStats}"
 
