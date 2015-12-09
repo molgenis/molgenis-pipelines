406e8754f1998eead5f728527a92276e82993ee0
RoanKanninga <roankanninga@gmail.com>
2015-12-09 08:37:09 +0100

    changed copying to results folder per project to copying to results per sample

--- compute5/NIPT/protocols/CopyToResultsDir.sh
+++ compute5/NIPT/protocols/CopyToResultsDir.sh
@@ -1,7 +1,7 @@
 #MOLGENIS walltime=23:59:00 nodes=1 cores=1 mem=4gb
 
 #Parameter mapping
-#string resultsDir
+#string resultsSampleDir
 #string intermediateDir
 #string reportHTML
 #string qcReportHTML
@@ -10,12 +10,14 @@
 # Change permissions
 umask 0007
 
+if [ ! -d ${resultsSampleDir}  ] 
+then
 
-cp ${reportPDF} ${resultsDir}
-cp ${reportHTML} ${resultsDir}
-cp ${qcReportPDF} ${resultsDir}
-cp ${qcReportHTML} ${resultsDir}
-echo "cp ${reportPDF} ${resultsDir}"
-echo "cp ${reportHTML} ${resultsDir}"
-echo "cp ${qcReportPDF} ${resultsDir}"
-echo "cp ${qcReportHTML} ${resultsDir}"
+cp ${reportPDF} ${resultsSampleDir}
+cp ${reportHTML} ${resultsSampleDir}
+cp ${qcReportPDF} ${resultsSampleDir}
+cp ${qcReportHTML} ${resultsSampleDir}
+echo "cp ${reportPDF} ${resultsSampleDir}"
+echo "cp ${reportHTML} ${resultsSampleDir}"
+echo "cp ${qcReportPDF} ${resultsSampleDir}"
+echo "cp ${qcReportHTML} ${resultsSampleDir}"
