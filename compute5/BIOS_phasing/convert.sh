25e0baea750812a55a0aa5e70f783fd7db819952
freerkvandijk <freerk.van.dijk@gmail.com>
2016-02-19 12:30:34 +0100

    Initial commit for folders and files BIOS phasing pipeline.

---
+++ compute5/BIOS_phasing/convert.sh
@@ -0,0 +1,6 @@
+sed '/^#/ d'< "$1" > "$1.tmp"
+awk '{printf "%s"",",$1}' FS="," "$1.tmp" > "$2"
+perl -pi -e 's/,$/\n/g' "$2" 
+awk '{printf "%s"",",$2}' FS="," "$1.tmp" >> "$2"
+perl -pi -e 's/,$/\n/g' "$2"
+rm "$1.tmp"
