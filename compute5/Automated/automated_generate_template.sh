3e738c0a15c273bea4f0c24a3edc27a8e3c94561
RoanKanninga <roankanninga@gmail.com>
2016-04-28 16:08:13 +0200

    pipeline is now automatically starting

--- compute5/Automated/automated_generate_template.sh
+++ compute5/Automated/automated_generate_template.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-##Choose parameters_calculon or parameters_zinc-finger
-ENVIRONMENT_PARAMETERS=parameters_zinc-finger.csv
+module load NGS_DNA/3.2.2-Molgenis-Compute-v16.04.1-Java-1.8.0_45
+module list 
+HOST=$(hostname)
+##Running script for checking the environment variables
+sh ${EBROOTNGS_DNA}/checkEnvironment.sh ${HOST}
+
+ENVIRONMENT_PARAMETERS=$(awk '{print $1}' ./environment_checks.txt)
+TMPDIR=$(awk '{print $2}' ./environment_checks.txt)
+GROUP=$(awk '{print $3}' ./environment_checks.txt)
+
 PROJECT=$1
-TMPDIR=$2
-WORKDIR="/groups/umcg-gaf/${TMPDIR}"
+WORKDIR="/groups/${GROUP}/${TMPDIR}"
 RUNID=run01
-## For small batchsize (6) leave BATCH empty, _exome (10 batches) or _wgs (20 batches)  OR  this is beta: _chr (per chrosomome), _NO (1 batch),
+## For small batchsize (6) leave BATCH empty, _exome (10 batches), _wgs (20 batches) or _chr (per chrosomome), OR but this is beta _NO (1 batch),
 BATCH="_chr"
 THISDIR=$(pwd)
 
@@ -16,7 +23,7 @@ if [ $SAMPLESIZE -gt 199 ]
 then
     	WORKFLOW=${EBROOTNGS_DNA}/workflow_samplesize_bigger_than_200.csv
 else
-        WORKFLOW=${EBROOTNGS_DNA}/workflow.csv
+    	WORKFLOW=${EBROOTNGS_DNA}/workflow.csv
 fi
 
 if [ -f .compute.properties ];
@@ -32,24 +39,27 @@ fi
 perl ${EBROOTNGS_DNA}/convertParametersGitToMolgenis.pl ${EBROOTNGS_DNA}/parameters.csv > \
 ${WORKDIR}/generatedscripts/${PROJECT}/out.csv
 
+perl ${EBROOTNGS_DNA}/convertParametersGitToMolgenis.pl ${EBROOTNGS_DNA}/parameters_${GROUP}.csv > \
+${WORKDIR}/generatedscripts/${PROJECT}/group_parameters.csv
 
 perl ${EBROOTNGS_DNA}/convertParametersGitToMolgenis.pl ${EBROOTNGS_DNA}/${ENVIRONMENT_PARAMETERS} > \
 ${WORKDIR}/generatedscripts/${PROJECT}/environment_parameters.csv
-module list
+
 sh $EBROOTMOLGENISMINCOMPUTE/molgenis_compute.sh \
 -p ${WORKDIR}/generatedscripts/${PROJECT}/out.csv \
 -p ${WORKDIR}/generatedscripts/${PROJECT}/environment_parameters.csv \
+-p ${WORKDIR}/generatedscripts/${PROJECT}/group_parameters.csv \
 -p ${EBROOTNGS_DNA}/batchIDList${BATCH}.csv \
 -p ${WORKDIR}/generatedscripts/${PROJECT}/${PROJECT}.csv \
--w ${EBROOTNGS_DNA}/automated_create_in-house_ngs_projects_workflow.csv \
+-w ${EBROOTNGS_DNA}/create_in-house_ngs_projects_workflow.csv \
 -rundir ${WORKDIR}/generatedscripts/${PROJECT}/scripts \
 --runid ${RUNID} \
 -o "workflowpath=${WORKFLOW};\
 outputdir=scripts/jobs;mainParameters=${WORKDIR}/generatedscripts/${PROJECT}/out.csv;\
+group=${WORKDIR}/generatedscripts/${PROJECT}/group_parameters.csv;\
 ngsversion=$(module list | grep -o -P 'NGS_DNA(.+)');\
 environment_parameters=${WORKDIR}/generatedscripts/${PROJECT}/environment_parameters.csv;\
 batchIDList=${EBROOTNGS_DNA}/batchIDList${BATCH}.csv;\
 worksheet=${WORKDIR}/generatedscripts/${PROJECT}/${PROJECT}.csv" \
 -weave \
 --generate
-
--- compute5/Automated/automated_startPipeline.sh
+++ compute5/Automated/automated_startPipeline.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
-set -e
+set -e 
 set -u
+
+groupname=$1
 
 MYINSTALLATIONDIR=$( cd -P "$( dirname "$0" )" && pwd )
 ##source config file (zinc-finger.gcc.rug.nl.cfg, leucine-zipper.gcc.rug.nl OR gattaca.cfg)
 myhost=$(hostname)
 echo $myhost
-
+. ${MYINSTALLATIONDIR}/${groupname}.cfg
 . ${MYINSTALLATIONDIR}/${myhost}.cfg
 . ${MYINSTALLATIONDIR}/sharedConfig.cfg
 
-ls ${SAMPLESHEETSDIR}/*.csv > ${SAMPLESHEETSDIR}/allSampleSheets_Zinc_startPipeline.txt
-
+ls ${SAMPLESHEETSDIR}/*.csv > ${SAMPLESHEETSDIR}/allSampleSheets_Zinc_startPipeline.txt 
 pipeline="dna"
-count=0
-echo "Logfiles will be written to $LOGDIR"
 
-for i in $(ls ${SAMPLESHEETSDIR}/*.csv)
+AUTOMATEDVERSION=$(module list | grep -o -P 'Automated(.+)' | awk -F"/" '{print $2}' )
+
+if [ "${pipeline}" == "dna" ] 
+then
+	if [ "${AUTOMATEDVERSION}" == "3.2.2" ]
+	then
+		module load NGS_DNA/${AUTOMATEDVERSION}-Molgenis-Compute-v16.04.1-Java-1.8.0_45
+	fi 
+fi
+
+count=0 echo "Logfiles will be written to $LOGDIR"
+
+for i in $(ls ${SAMPLESHEETSDIR}/*.csv) 
 do
+	echo "$i"
   	csvFile=$(basename $i)
         filePrefix="${csvFile%.*}"
 
 	HEADER=$(head -1 ${i})
+	head -2 ${i} | tail -1 > ${i}.tmp
 	OLDIFS=$IFS
 	IFS=','
 	array=($HEADER)
@@ -33,12 +46,11 @@ do
 	do
   		if [ "${j}" == "project" ]
   	     	then
-  	        	myproject=$(awk '{FS=","}{print $'$count'}' $i | uniq)
-  	      	fi
+  	        	myproject=$(awk -F"," '{print $'$count'}' ${i}.tmp)
+  	 	fi
 		count=$((count + 1))
 	done
-
-	echo "myproject: $myproject"
+	rm ${i}.tmp
         FINISHED="no"
         OLDIFS=$IFS
         IFS=_
@@ -47,12 +59,6 @@ do
         run=$3
 	IFS=$OLDIFS
         LOGGER=${LOGDIR}/${myproject}.startPipeline.logger
-
-        if [ -f ${LOGDIR}/${myproject}.startPipeline.locked ]
-        then
-            	exit 0
-        fi
-
 	if [[ -f $LOGDIR/${filePrefix}.dataCopiedToZinc && ! -f $LOGDIR/${myproject}.pipeline.started ]]
         then
                 ### Step 4: Does the pipeline need to run?
@@ -74,14 +80,14 @@ do
 				underline=`tput smul`
 				normal=`tput sgr0`
 				bold=`tput bold`
-				printf "${bold}WARNING: there is no pipeline version loaded, this can be because this script is run manually.\nA default version of the NGS_DNA pipeline will be loaded!\n\n" 
+				printf "${bold}WARNING: there is no pipeline version loaded, this can be because this script is run manually.\nA default version of the NGS_DNA pipeline will be loaded!\n\n"
 				module load $DNA
 				pipelineVersion=$(module list | grep -o -P 'NGS_DNA(.+)')
 				printf "The version which is now loaded is $pipelineVersion${normal}\n\n"
 			fi
                         mkdir -p ${GENERATEDSCRIPTS}/${run}_${sequencer}/
-			echo "copying $EBROOTNGS_DNA/automated/automated_generate_template.sh to ${GENERATEDSCRIPTS}/${run}_${sequencer}/generate.sh" >> $LOGGER
-                        cp $EBROOTNGS_DNA/automated/automated_generate_template.sh ${GENERATEDSCRIPTS}/${run}_${sequencer}/generate.sh
+			echo "copying $EBROOTAUTOMATED/automated_generate_template.sh to ${GENERATEDSCRIPTS}/${run}_${sequencer}/generate.sh" >> $LOGGER
+                        cp ${EBROOTAUTOMATED}/automated_generate_template.sh ${GENERATEDSCRIPTS}/${run}_${sequencer}/generate.sh
 			if [ -f ${GENERATEDSCRIPTS}/${run}_${sequencer}/${run}_${sequencer}.csv ]
 			then
 				echo "${GENERATEDSCRIPTS}/${run}_${sequencer}/${run}_${sequencer}.csv already existed, will now be removed and will be replaced by a fresh copy" >> $LOGGER
@@ -90,7 +96,7 @@ do
 
 			cp ${SAMPLESHEETSDIR}/${csvFile} ${GENERATEDSCRIPTS}/${run}_${sequencer}/${run}_${sequencer}.csv
 			cd ${GENERATEDSCRIPTS}/${run}_${sequencer}/
-			sh ${GENERATEDSCRIPTS}/${run}_${sequencer}/generate.sh "${run}_${sequencer}" "${TMPDIR}"
+			sh ${GENERATEDSCRIPTS}/${run}_${sequencer}/generate.sh "${run}_${sequencer}"
 			cd scripts
 
 			touch ${GENERATEDSCRIPTS}/${run}_${sequencer}/scripts/CopyPrmTmpData_0.sh.finished
@@ -99,12 +105,17 @@ do
 			WHOAMI=$(whoami)
 			HOSTN=$(hostname)
 			pipelineVersion=$(module list | grep -o -P 'NGS_DNA(.+)')
-			printf "Pipeline: ${pipelineVersion}\nStarttime:`date +%d/%m/%Y` `date +%H:%M`\nProject: $PROJECT\nStarted by: $WHOAMI\nHost: ${HOSTN}\n\nProgress can be followed via the command squeue -u $WHOAMI on $HOSTN.\nYou will receive an email when the pipeline is finished!\n\nCheers from the GCC :)"| mail -s "NGS_DNA pipeline is started for project $PROJECT on `date +%d/%m/%Y` `date +%H:%M`" ${ONTVANGER}
 
+			cd ${PROJECTSDIR}/${PROJECT}/run01/jobs/
+			sh submit.sh
+
+			touch ${LOGDIR}/${PROJECT}.pipeline.started
+
+			printf "Pipeline: ${pipelineVersion}\nStarttime:`date +%d/%m/%Y` `date +%H:%M`\nProject: $PROJECT\nStarted by: $WHOAMI\nHost: ${HOSTN}\n\nProgress can be followed via the command squeue -u $WHOAMI on $HOSTN.\nYou will receive an email when the pipeline is finished!\n\nCheers from the GCC :)"| mail -s "NGS_DNA pipeline is started for project $PROJECT on `date +%d/%m/%Y` `date +%H:%M`" ${ONTVANGER}
+			sleep 30
 		else
               		echo "Pipeline is skipped" >> ${LOGGER}
                 fi
         fi
-	rm ${LOGDIR}/${myproject}.startPipeline.locked
-	
+
 done
