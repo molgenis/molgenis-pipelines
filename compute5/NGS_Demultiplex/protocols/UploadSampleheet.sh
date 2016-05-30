16dbefeba76daed453f324756a1c8686feb816d4
gerbenvandervries <gerbenvandervries@gmail.com>
2016-05-30 13:36:28 +0200

    automatic upload samplessheets

--- compute5/NGS_Demultiplex/protocols/UploadSampleheet.sh
+++ compute5/NGS_Demultiplex/protocols/UploadSampleheet.sh
@@ -9,6 +9,8 @@ WHOAMI=$(whoami)
 
 echo "Importing Samplesheet into ${MOLGENISSERVER}"
 
+cp ${sampleSheet} ${MCsampleSheet} 
+
 CURLRESPONSE=$(curl -H "Content-Type: application/json" -X POST -d "{"username"="${USERNAME}", "password"="${PASSWORD}"}" https://${MOLGENISSERVER}/api/v1/login)
 TOKEN=${CURLRESPONSE:10:32}
-curl -H "x-molgenis-token:${TOKEN}" -X POST -F"file=@${sampleSheet}" -Faction=add -Fnotify=false https://${MOLGENISSERVER}/plugin/importwizard/importFile
+curl -H "x-molgenis-token:${TOKEN}" -X POST -F"file=@${MCsampleSheet}" -Faction=add -Fnotify=false https://${MOLGENISSERVER}/plugin/importwizard/importFile
