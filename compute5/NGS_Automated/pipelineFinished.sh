7e2232b0525e69dbe777b99d1203f731c41a1040
RoanKanninga <roankanninga@gmail.com>
2016-09-01 14:26:36 +0200

    bug fixes in pipelineFinished script

--- compute5/NGS_Automated/pipelineFinished.sh
+++ compute5/NGS_Automated/pipelineFinished.sh
@@ -22,22 +22,53 @@ for i in ${ALLFINISHED[@]}
 do
 	filename=$(basename $i)
 	projectName="${filename%%.*}"
-	for i in $(ls ${PROJECTSDIR}/${projectName}/*/rawdata/ngs/*); do if [ -L $i ];then readlink $i > ${LOGDIR}/${projectName}/${projectName}.rawdatalink ; fi;done
 
-	while read line ; do dirname $line > ${LOGDIR}/${projectName}/${projectName}.rawdatalinkDirName; done<${LOGDIR}/${projectName}/${projectName}.rawdatalink
+	## Check which rawdata belongs to the project
+	for i in $(ls ${PROJECTSDIR}/${projectName}/*/rawdata/ngs/*.md5)
+ 	do 
+		if [ -L $i ]
+		then 
+			readlink $i > ${LOGDIR}/${projectName}/${projectName}.rawdatalink 
+		fi
+	done
+	## if md5sums for are not present try to fix it via the fq.gz
+	if [ ! -f  ${LOGDIR}/${projectName}/${projectName}.rawdatalink ]
+        then
+                for i in $(ls ${PROJECTSDIR}/${projectName}/*/rawdata/ngs/*.fq.gz)
+                do
+                        if [ -L $i ]
+                        then
+                                readlink $i > ${LOGDIR}/${projectName}/${projectName}.rawdatalink
+
+                        fi
+                done
+        fi
+
+	rawdataName=""
+        if [  -f  ${LOGDIR}/${projectName}/${projectName}.rawdatalink ]
+        then
+		while read line 
+		do 
+			dirname $line > ${LOGDIR}/${projectName}/${projectName}.rawdatalinkDirName 
+		done<${LOGDIR}/${projectName}/${projectName}.rawdatalink
 
 		rawDataName=$(while read line ; do basename $line ; done<${LOGDIR}/${projectName}/${projectName}.rawdatalinkDirName)
+	fi
 
-		echo "moving ${projectName} files to ${LOGDIR}/${projectName}/ and removing tmp finished files"
-		if [[ -f ${LOGDIR}/${projectName}/${projectName}.pipeline.logger  && -f ${LOGDIR}/${projectName}/${projectName}.pipeline.started && -f ${LOGDIR}/${projectName}/${projectName}.rawdatalink && -f ${LOGDIR}/${projectName}/${projectName}.rawdatalinkDirName ]]
-		then 
+	echo "moving ${projectName} files to ${LOGDIR}/${projectName}/ and removing tmp finished files"
+	if [[ -f ${LOGDIR}/${projectName}/${projectName}.pipeline.logger  && -f ${LOGDIR}/${projectName}/${projectName}.pipeline.started ]]
+	then 
+		if [ -f ${LOGDIR}/${projectName}/${projectName}.rawdatalink && -f ${LOGDIR}/${projectName}/${projectName}.rawdatalinkDirName ]
+		then
 			touch ${LOGDIR}/${projectName}/${rawDataName}
-			mv ${LOGDIR}/${projectName}.pipeline.finished ${LOGDIR}/${projectName}/
-
-		else
-			echo "there is/are missing some files:${projectName}.pipeline.logger or  ${projectName}.pipeline.started or ${projectName}/${projectName}.rawdatalink or ${projectName}.rawdatalinkDirName"
-			echo "there is/are missing some files:${projectName}.pipeline.logger or  ${projectName}.pipeline.started or ${projectName}/${projectName}.rawdatalink or ${projectName}.rawdatalinkDirName" >> ${LOGDIR}/${projectName}/${projectName}.pipeline.logger
 		fi
+	
+		mv ${LOGDIR}/${projectName}.pipeline.finished ${LOGDIR}/${projectName}/
+
+	else
+		echo "there is/are missing some files:${projectName}.pipeline.logger or  ${projectName}.pipeline.started"
+		echo "there is/are missing some files:${projectName}.pipeline.logger or  ${projectName}.pipeline.started" >> ${LOGDIR}/${projectName}/${projectName}.pipeline.logger
+	fi
 	if [ ! -f ${LOGDIR}/${projectName}/${projectName}.pipeline.finished.mailed ]
 	then
 		printf "The results can be found: ${PROJECTSDIR}/${projectName} \n\nCheers from the GCC :)"| mail -s "NGS_DNA pipeline is finished for project ${projectName} on `date +%d/%m/%Y` `date +%H:%M`" ${ONTVANGER}
