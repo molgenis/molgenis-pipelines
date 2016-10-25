4a6de11c144279ee6bda5bb9353102c23adbf799
freerkvandijk <freerk.van.dijk@gmail.com>
2016-10-25 18:08:51 +0200

    Start of GATK read counter protocols

---
+++ compute5/ASE/protocols/ASEReadCounter.sh
@@ -0,0 +1,57 @@
+#MOLGENIS nodes=1 ppn=2 mem=10gb walltime=3-10:00:00
+
+### variables to help adding to database (have to use weave)
+#string project
+###
+#string stage
+#string checkStage
+#string CHR
+#string onekgGenomeFasta
+#string bam
+#string selectVariantsBiallelicSNPsVcf
+#string gatkVersion
+#string ASEReadCountsDir
+#string ASEReadCountsChrOutput
+
+
+
+echo "## "$(date)" Start $0"
+
+
+getFile ${onekgGenomeFasta}
+getFile ${bam}
+getFile ${selectVariantsBiallelicSNPsVcf}
+getFile ${selectVariantsBiallelicSNPsVcf}.idx
+
+${stage} GATK/${gatkVersion}
+${checkStage}
+
+mkdir -p ${ASEReadCountsDir}
+
+if java -Xmx8g -XX:ParallelGCThreads=2 -Djava.io.tmpdir=${TMPDIR} -jar ${EBROOTGATK}/GenomeAnalysisTK.jar \
+ -T ASEReadCounter \
+ -R ${onekgGenomeFasta} \
+ -o ${ASEReadCountsChrOutput} \
+ -I ${bam} \
+ -sites ${selectVariantsBiallelicSNPsVcf} \
+ -L ${selectVariantsBiallelicSNPsVcf} \
+ -U ALLOW_N_CIGAR_READS \
+ -minDepth 10 \
+ --minMappingQuality 10 \
+ --minBaseQuality 2
+
+#-drf DuplicateRead #Add this parameter to NOT remove duplicate reads
+
+then
+ echo "returncode: $?"; 
+
+ putFile ${ASEReadCountsChrOutput}
+ echo "succes moving files";
+else
+ echo "returncode: $?";
+ echo "fail";
+fi
+
+echo "## "$(date)" ##  $0 Done "
+
+
---
+++ compute5/ASE/protocols/SelectVariants.sh
@@ -0,0 +1,47 @@
+#MOLGENIS nodes=1 ppn=2 mem=10gb walltime=3-10:00:00
+
+### variables to help adding to database (have to use weave)
+#string project
+###
+#string stage
+#string checkStage
+#string CHR
+#string onekgGenomeFasta
+#string VCFprefix
+#string VCFsuffix
+#string gatkVersion
+#string selectVariantsDir
+#string VCFinputDir
+#string selectVariantsBiallelicSNPsVcf
+
+echo "## "$(date)" Start $0"
+
+
+getFile ${onekgGenomeFasta}
+getFile ${VCFprefix}${CHR}${VCFsuffix}
+
+
+${stage} GATK/${gatkVersion}
+${checkStage}
+
+mkdir -p ${selectVariantsDir}
+
+
+# Extract only bi-allelic sites from VCF
+if java -Xmx8g -XX:ParallelGCThreads=2 -Djava.io.tmpdir=${TMPDIR} -jar ${EBROOTGATK}/GenomeAnalysisTK.jar \
+ -T SelectVariants \
+ -R ${onekgGenomeFasta} \
+ -V ${VCFinputDir}/${VCFprefix}${CHR}${VCFsuffix} \
+ -o ${selectVariantsBiallelicSNPsVcf} \
+ -restrictAllelesTo BIALLELIC
+then
+ echo "returncode: $?"; 
+ putFile ${selectVariantsBiallelicSNPsVcf}
+ putFile ${selectVariantsBiallelicSNPsVcf}.idx
+ echo "succes moving files";
+else
+ echo "returncode: $?";
+ echo "fail";
+fi
+
+echo "## "$(date)" ##  $0 Done "
