4fa9057f4c518f74983ad234b891f91ce6243381
npklein <niekdeklein@gamail.com>
2016-11-04 13:31:41 +0100

    add chromosomes file

---
+++ compute5/BIOS_phasing/exampleGenerateScript.sh
@@ -0,0 +1,11 @@
+module load Molgenis-Compute/v16.05.1-Java-1.8.0_45
+sh $EBROOTMOLGENISMINCOMPUTE/molgenis_compute.sh \
+  --backend slurm \
+  --generate \
+  --header templates/slurm/header.ftl \
+  -p parameters.converted.csv \
+  -p samplesheet.csv \
+  -w workflow.csv \
+  -p chromosomes_X_Y.csv \
+  -p chromosome_chunks.txt \
+  -rundir jobs/ --weave
