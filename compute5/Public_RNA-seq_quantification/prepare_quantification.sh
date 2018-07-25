409ee521ab3b8f9ff21288de00b3fab7c41aaa35
npklein <niekdeklein@gmail.com>
2018-07-25 10:24:52 +0200

    small change prepare script

--- compute5/Public_RNA-seq_quantification/prepare_quantification.sh
+++ compute5/Public_RNA-seq_quantification/prepare_quantification.sh
@@ -2,9 +2,9 @@ module load Molgenis-Compute/v16.05.1-Java-1.8.0_45
 sh $EBROOTMOLGENISMINCOMPUTE/molgenis_compute.sh \
   --backend slurm \
   --generate \
-  -p parameters/parameters.converted.csv \
+  -p parameter_files/parameters.converted.csv \
   -p samplesheet.csv \
-  -w workflow.csv \
+  -w workflows/workflow.csv \
   -rundir results/ \
   --weave
 
