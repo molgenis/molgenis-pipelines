8f6011578660f56e4fcbb789d8637bfbff5c59cb
npklein <niekdeklein@gmail.com>
2018-07-25 18:39:55 +0200

    longer walltime

--- compute5/Public_RNA-seq_quantification/protocols/Sailfish.sh
+++ compute5/Public_RNA-seq_quantification/protocols/Sailfish.sh
@@ -1,4 +1,4 @@
-#MOLGENIS nodes=1 ppn=1 mem=10gb walltime=05:59:00
+#MOLGENIS nodes=1 ppn=1 mem=10gb walltime=23:59:00
 
 ### variables to help adding to database (have to use weave)
 #string internalId
