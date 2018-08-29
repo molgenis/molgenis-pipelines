5c0f073437e9188482ee1634a89c15e818d3fdc5
npklein <niekdeklein@gmail.com>
2018-08-29 16:32:23 +0200

    convert original fastq to cram instead of alligned fastq to cram

--- compute5/Public_RNA-seq_QC/protocols/RemoveFastqFiles.sh
+++ compute5/Public_RNA-seq_QC/protocols/RemoveFastqFiles.sh
@@ -33,71 +33,87 @@ echo "## "$(date)" Start $0"
 echo "ID (internalId-project-sampleName): ${internalId}-${project}-${sampleName}"
 
 #Run scramble on 2 cores to do BAM -> CRAM conversion
-echo "Starting scramble CRAM to BAM conversion";
+echo "Convert fastq to unaligned BAM";
+picard FastqToSam \
+        F1=$reads1FqGz \
+        F2=$reads2FqGz \
+        O=$TMPDIR/${uniqueID}.bam \
+        SM=$uniqueID
 
-if scramble \
-    -I cram \
-    -O bam \
-    -m \
-    -r ${onekgGenomeFasta} \
-    ${cramFileDir}${uniqueID}.cram \
-    $TMPDIR/${uniqueID}.bam \
-    -t 4
+returnCode=$?
+echo "returncode: $returnCode";
+if [ $returnCode -eq 0 ]
 then
-  echo "Starting BAM to FASTQ conversion: sort BAM file";
-  samtools sort \
+  echo "convert fastq to unalligned BAM successful"
+else
+  echo "ERROR: couldn't convert to fastq to unalligned BAM"
+  exit 1;
+fi
+  
+echo "Convert unaligned BAM to CRAM";
+samtools view -T $onekgGenomeFasta \
+                -C \
+                -o  ${cramFileDir}${uniqueID}.cram \ 
+                $TMPDIR/${uniqueID}.bam
+returnCode=$?
+echo "returncode: $returnCode";
+if [ $returnCode -eq 0 ]
+then
+  echo "convert to cram successful"
+else
+  echo "ERROR: couldn't convert to cram"
+  exit 1;
+fi
+                
+echo "Convert CRAM to fastq";
+fq1NameGz=$(basename $reads1FqGz)
+fq1Name=${fq1NameGz%.gz}
+fq2NameGz=$(basename $reads2FqGz)
+fq2Name=${fq2NameGz%.gz}
+samtools fastq \
     -@ 4 \
-    -n \
-    -o $TMPDIR/${uniqueID}.sorted.bam \
-    $TMPDIR/${uniqueID}.bam 
-  echo "Starting BAM to FASTQ conversion: convert sorted BAM file";
-  fq1NameGz=$(basename $reads1FqGz)
-  fq1Name=${fq1NameGz%.gz}
-  fq2NameGz=$(basename $reads2FqGz)
-  fq2Name=${fq2NameGz%.gz}
+    -1 $TMPDIR/$fq1Name \
+    -2 $TMPDIR/$fq2Name \
+    $TMPDIR/${uniqueID}.bam
+returnCode=$?
+echo "returncode: $returnCode";
+if [ $returnCode -eq 0 ]
+then
+  echo "convert CRAM to fastq successful"
+else
+  echo "ERROR: couldn't convert cram to fastq"
+  exit 1;
+fi
+
+
+echo "count fastq lines"
+fastq1Lines=$(wc -l $TMPDIR/$fq1Name)
+fastq2Lines=$(wc -l $TMPDIR/$fq2Name)
+echo "fastq1Lines: $fastq1Lines"
+echo "fastq2Lines: $fastq2Lines"
+
+echo "count original fastq lines"
+originalFastq1Lines=$(wc -l ${reads1FqGz})
+originalFastq2Lines=$(wc -l ${reads2FqGz})
+
+echo "originalFastq1Lines: $originalFastq1Lines"
+echo "originalFastq2lines: $originalFastq2lines"
+  
+if [ "$originalFastq1Lines" -eq "$fastq1Lines" ];
+then
+  echo "Fastq1 same number of lines"
   if [ ${#reads2FqGz} -eq 0 ]; 
   then
-    samtools fastq \
-      -@ 4 \
-      -0 $TMPDIR/$fq1Name \
-      $TMPDIR/${uniqueID}.sorted.bam
-    echo "count fastq lines"
-    fastq1Lines=$(wc -l $TMPDIR/$fq1Name)
-    echo "fastq1Lines: $fastq1Lines"
-  else
-    samtools fastq \
-      -@ 4 \
-      -1 $TMPDIR/$fq1Name \
-      -2 $TMPDIR/$fq2Name \
-      $TMPDIR/${uniqueID}.sorted.bam
-
-    echo "count fastq lines"
-    fastq1Lines=$(wc -l $TMPDIR/$fq1Name)
-    fastq2Lines=$(wc -l $TMPDIR/$fq2Name)
-    echo "fastq1Lines: $fastq1Lines"
-    echo "fastq2Lines: $fastq2Lines"
-    originalFastq2lines=$(wc -l ${reads2FqGz})
-  fi
-  echo "count original fastq lines"
-  originalFastq1Lines=$(wc -l ${reads1FqGz})
-
-  echo "originalFastq1Lines: $originalFastq1Lines"
-  echo "originalFastq2lines: $originalFastq2lines"
-  if [ "$originalFastq1Lines" -eq "$fastq1Lines" ];
-  then
-    echo "Fastq1 same number of lines"
-    if [ ${#reads2FqGz} -eq 0 ]; 
+  if [ "$originalFastq2Lines" -eq "$fastq2Lines" ];
     then
-      if [ "$originalFastq2Lines" -eq "$fastq2Lines" ];
-      then
-        echo "Fastq2 same number of lines"
-        echo "rm $reads2FqGz"
-      else
-        echo "ERROR: Fastq2 not same number of lines"
-        exit 1;
-      fi
+      echo "Fastq2 same number of lines"
+      echo "rm $reads2FqGz"
+    else
+      echo "ERROR: Fastq2 not same number of lines"
+      exit 1;
     fi
-    echo "rm $reads1FqGz"
+  fi
+  echo "rm $reads1FqGz"
   else
     echo "ERROR: Fastq1 not same number of lines"
     exit 1;
