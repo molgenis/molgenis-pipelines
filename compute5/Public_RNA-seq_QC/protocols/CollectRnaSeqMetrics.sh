b98299438e47eb6af64e55ada589b4b629f7c423
npklein <niekdeklein@gmail.com>
2019-01-15 10:44:06 +0100

    change collect* to collectQc* to differentiate this with the one in the genotypeCalling pipeline

--- compute5/Public_RNA-seq_QC/protocols/CollectRnaSeqMetrics.sh
+++ compute5/Public_RNA-seq_QC/protocols/CollectRnaSeqMetrics.sh
@@ -11,9 +11,9 @@
 #string picardVersion
 #string sortedBam
 #string sortedBai
-#string collectRnaSeqMetricsDir
-#string collectRnaSeqMetrics
-#string collectRnaSeqMetricsChart
+#string collectRnaSeqQcMetricsDir
+#string collectRnaSeqQcMetrics
+#string collectRnaSeqQcMetricsChart
 #string genesRefFlat
 #string rRnaIntervalList
 #string onekgGenomeFasta
@@ -28,28 +28,28 @@ ${stage} R/${RVersion}
 #Check modules
 ${checkStage}
 
-mkdir -p ${collectRnaSeqMetricsDir}
+mkdir -p ${collectRnaSeqQcMetricsDir}
 
 echo "## "$(date)" Start $0"
 echo "ID (internalId-project-sampleName): ${internalId}-${project}-${sampleName}"
 
 java -Xmx8g -XX:ParallelGCThreads=4 -jar $EBROOTPICARD/picard.jar CollectRnaSeqMetrics \
  INPUT=${sortedBam} \
- OUTPUT=${collectRnaSeqMetrics} \
- CHART_OUTPUT=${collectRnaSeqMetricsChart} \
+ OUTPUT=${collectRnaSeqQcMetrics} \
+ CHART_OUTPUT=${collectRnaSeqQcMetricsChart} \
  METRIC_ACCUMULATION_LEVEL=SAMPLE \
  METRIC_ACCUMULATION_LEVEL=READ_GROUP \
  REFERENCE_SEQUENCE=${onekgGenomeFasta} \
  REF_FLAT=${genesRefFlat} \
  RIBOSOMAL_INTERVALS=${rRnaIntervalList} \
  STRAND_SPECIFICITY=NONE \
- TMP_DIR=${collectRnaSeqMetricsDir}
+ TMP_DIR=${collectRnaSeqQcMetricsDir}
 
 echo "returncode: $?";
 
-cd ${collectRnaSeqMetricsDir}
-md5sum $(basename ${collectRnaSeqMetrics}) > $(basename ${collectRnaSeqMetrics}).md5
-md5sum $(basename ${collectRnaSeqMetricsChart}) > $(basename ${collectRnaSeqMetricsChart}).md5
+cd ${collectRnaSeqQcMetricsDir}
+md5sum $(basename ${collectRnaSeqQcMetrics}) > $(basename ${collectRnaSeqMetrics}).md5
+md5sum $(basename ${collectRnaSeqQcMetricsChart}) > $(basename ${collectRnaSeqMetricsChart}).md5
 cd -
 
 echo "## "$(date)" ##  $0 Done "
