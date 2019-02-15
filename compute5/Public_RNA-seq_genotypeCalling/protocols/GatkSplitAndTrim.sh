edc78767e5d95afdffcfc4f0d027753eb882c55c
npklein <niekdeklein@gmail.com>
2019-02-15 10:47:11 +0100

    Revert "--fix_misencoded_quality_scores and --allow_potentially_misencoded_quality_scores are not options of SplitNTrim in GATK4, so this is not relevant anymore"
    This reverts commit 87ce41285cd52b787f5fcd858bb2148bed046264.

--- compute5/Public_RNA-seq_genotypeCalling/protocols/GatkSplitAndTrim.sh
+++ compute5/Public_RNA-seq_genotypeCalling/protocols/GatkSplitAndTrim.sh
@@ -27,6 +27,37 @@ ${checkStage}
 
 mkdir -p ${splitAndTrimDir}
 
+#it botches on base quality scores use --allow_potentially_misencoded_quality_scores / the tool is not paralel with nt/nct
+qualAction=$(samtools view ${markDuplicatesBam} | \
+    head -1000000 | \
+    awk '{gsub(/./,"&\n",$11);print $11}'| \
+    sort -u| \
+    perl -wne '
+    $_=ord($_);
+    print $_."\n"if(not($_=~/10/));' | \
+    sort -n | \
+    perl -wne '
+    use strict;
+    use List::Util qw/max min/;
+    my @ords=<STDIN>;
+    if(min(@ords) >= 59 && max(@ords) <=104 ){
+      print " --fix_misencoded_quality_scores ";
+      warn "Illumina <= 1.7 scores detected using:--fix_misencoded_quality_scores.\n";
+    }elsif(min(@ords) >= 33 && max(@ords) <= 74){
+	  print " ";
+	  warn "quals > illumina 1.8 detected no action to take.\n";
+    }elsif(min(@ords) >= 33 && max(@ords) <= 83){
+	  print " --allow_potentially_misencoded_quality_scores ";
+	  warn "Strange illumina like quals detected using:--allow_potentially_misencoded_quality_scores."
+    }else{
+	  die "Cannot estimate quality scores here is the list:".join(",",@ords)."\n";
+    }
+  ')
+
+echo
+echo "## Action to perform in quals: "$qualAction" ##"
+echo
+
 java -Xmx8g -XX:ParallelGCThreads=8 -Djava.io.tmpdir=${TMPDIR} -jar $EBROOTGATK/GenomeAnalysisTK.jar \
  -T SplitNCigarReads \
  -R ${onekgGenomeFasta} \
@@ -35,7 +66,8 @@ java -Xmx8g -XX:ParallelGCThreads=8 -Djava.io.tmpdir=${TMPDIR} -jar $EBROOTGATK/
  -rf ReassignOneMappingQuality \
  -RMQF 255 \
  -RMQT 60 \
- -U ALLOW_N_CIGAR_READS
+ -U ALLOW_N_CIGAR_READS \
+ $qualAction
 
 echo "returncode: $?";
 
